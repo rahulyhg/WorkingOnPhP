e9c3dd7e9e4302c230ffe25557d22d9b7a815f29
ManuDeveloper <manuepeva@gmail.com>
2018-03-08 19:24:41 -0500

    merging

--- Apache2.4/httpd-2.4.29-Win64-VC15/Apache24/htdocs/cuscohtml.php
+++ Apache2.4/httpd-2.4.29-Win64-VC15/Apache24/htdocs/cuscohtml.php
@@ -1,4 +1,9 @@
 <?php
-echo "doce";
 
+$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb;', 'root', '');
+$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb;', 'root', '');
+$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb;', 'root', '');
+$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb;', 'root', '');$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb;', 'root', '');
+$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb;', 'root', '');
+$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb;', 'root', '');
 php?>
