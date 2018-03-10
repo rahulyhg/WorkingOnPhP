f3ea6c2b8f950b92e4e12a9e12889bc498b7140c
ManuDeveloper <manuepeva@gmail.com>
2018-03-10 15:40:19 -0500

    merging

--- Apache2.4/httpd-2.4.29-Win64-VC15/Apache24/htdocs/cuscohtml.php
+++ Apache2.4/httpd-2.4.29-Win64-VC15/Apache24/htdocs/cuscohtml.php
@@ -1,2 +1,21 @@
 
-<?php phpinfo(); ?>
+<?
+	try{
+		$conn = new PDO("sqlsrv:Server=.\SQLEXPRESS;Database=mydb;
+			null, null");
+		$conn->setAttribute( PDO::ATR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+
+	catch(PDOException $e){
+		die("Error connecting to Server\n");
+	}
+
+	echo "connected to sql SErvern";
+
+	$query = 'select * from employees';
+	$stmt = $conn->query( $query);
+	while($row = $stmt->fetch( PDO::FETCH_ASSOC)){
+		print_r($row);
+	}
+
+?>
