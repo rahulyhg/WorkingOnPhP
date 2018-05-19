07f25491baf07167c52c5bab7ce0d82ef9d6f3dd
ManuDeveloper <manuepeva@gmail.com>
2018-05-19 17:53:08 -0500

    commiting changes in the file php.necesario, connecting throught pdo

--- necesario.php
+++ necesario.php
@@ -30,25 +30,25 @@
 
 		
 
-		try {
-			$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb', 
-					'root', 'gatubelo=00');
-			$handler->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $e) {
-			echo $e->getMessage();
-		}
+		// try {
+		// 	$handler = new PDO('mysql:host=127.0.0.1;dbname=mydb', 
+		// 			'root', 'gatubelo=00');
+		// 	$handler->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// } catch (PDOException $e) {
+		// 	echo $e->getMessage();
+		// }
 
 
 
 
  ?>
-<!-- 
+
  <!DOCTYPE html>
  <html>
  <head>
  	<title>To Do List</title>
- 	<link rel="stylesheet" href="/main.css"> -->
- <!-- 	<style type="text/css">
+ 	<link rel="stylesheet" href="/main.css">
+ 	<style type="text/css">
  		body {
 	background-color: #eceff0;
 }
@@ -63,27 +63,26 @@ a{
 	text-decoration: none;
 	border-bottom: 1px dashed #363639;
 }
- 	</style> -->
- <!-- </head>
+ 	</style>
+ </head>
  <body>
  		<div class="list">
- 			<h1 class="header">To Do.</h1> -->
- 		<!-- 	<?php if(!empty($items)): ?>
+ 			<h1 class="header">To Do.</h1>
+ 			<?php if(!empty($items)): ?>
  				<ul class="items">
  					<?php foreach($items as $item): ?>
  					<li><span class="item"><?php echo $item['name']; ?></span></li>
  					<li><span class="item done">Learn PhP.</span></li>
  				<?php endforeach; ?>
  				</ul>
- 			<?php  else: ?>
- 				<p>You Haven´t added items yet...!</p>
- 			<?php endif; ?> -->
+ 			
+ 			<?php endif; ?>
 
- 		<!-- 		<form action="add.php" method="post" class="item-add">
+ 				<form action="add.php" method="post" class="item-add">
  					<input type="text" name="name" placeholder="Type a new item here please." class="input" autocomplete="off" required>
  					 <input type="submit" value="add" class="submit">
  				</form>
 
  		</div>
  </body>
- </html> -->
+ </html>
