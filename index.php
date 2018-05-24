205adb7d63711e9ee2436ec7e93bfa40139386b9
ManuDeveloper <manuepeva@gmail.com>
2018-05-23 19:36:58 -0500

    commiting

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php 
-	
-    // require_once ''
-
-    // echo "alguien";
-    //  echo "alguien"; echo "alguien";
-    //   echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien"; echo "alguien";
-
-      // print_r(PDO::getAvailableDrivers());
-    // phpinfo();
-    // print_r(get_loaded_extensions()); 
-
-        // session_start();
-        // $_SESSION['user_id'] = 1;
+        session_start();
+        $_SESSION['user_id'] = 1;
         if ($db = new PDO('mysql:dbname=mydb;host=localhost', 
         'root', 'gatubelo=00')) {
             echo "success";
