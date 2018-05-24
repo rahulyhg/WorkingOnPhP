d5ad18a80cb8c4712c5b2efa896f839f3417a3e0
ManuDeveloper <manuepeva@gmail.com>
2018-05-23 19:56:55 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -31,50 +31,12 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>title</title>
 </head>
 <body>
-		<div class="list">
- 			<h1 class="header">To Do.</h1>
- 			<?php if(!empty($items)): ?>
- 				<ul class="items">
- 					<?php foreach($items as $item): ?>
- 					<li><span class="item"><?php echo $item['name']; ?></span></li>
- 					<li><span class="item done">Learn PhP.</span></li>
- 				<?php endforeach; ?>
- 				</ul>
- 			<?php  else: ?>
- 				<p>You Haven´t added items yet...!</p>
- 			<?php endif; ?>
-
- 				<form action="add.php" method="post" class="item-add">
- 					<input type="text" name="name" placeholder="Type a new item here please." class="input" autocomplete="off" required>
- 					 <input type="submit" value="add" class="submit">
- 				</form>
-
- 		</div>
+		
 </body>
 </html>
