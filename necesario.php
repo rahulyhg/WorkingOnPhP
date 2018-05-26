a5e4dd275fec6c4f6093af754a6a86e48f69e7a5
ManuDeveloper <manuepeva@gmail.com>
2018-05-25 20:06:34 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -5,22 +5,25 @@
 </head>
 <body>
 <?php 
-		echo $_POST['fname'];
-		echo "<br />";
-		echo $_POST['iname'];
-		echo "<br />";
-		echo $_POST['about'];
+		// echo $_POST['fname'];
+		// echo "<br />";
+		// echo $_POST['iname'];
+		// echo "<br />";
+		// echo $_POST['about'];
 
 ?>
 
-<form action="necesario.php" method="post">
-	First Name: <input type="text" name="fname"><br />
-	Last Name: <input type="text" name="iname"><br />
-	About: <textarea name="about"></textarea><br />
+		<div style="margin:3em;"> 
+			<div><form action="necesario.php" method="post">
+			First Name: <input type="text" name="fname"><br />
+			Last Name: <input type="text" name="iname"><br />
+			About: <textarea name="about"></textarea><br />
 
 	<input type="reset" name="reset">
 
 	<input type="submit" name="submit" value="Go">
-</form>
+</form></div>
+		</div>
+
 </body>
 </html>
