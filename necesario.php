3957ebf6e7023c54296c5ddcc004c84a35cb4ed1
ManuDeveloper <manuepeva@gmail.com>
2018-05-27 11:19:31 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -1,22 +1,25 @@
 
 <?php
 		// require_once('index.php');
-		$link = 'mysql:host=localhost;dbname=mydb';
-		$usuario =  'root';
-		$pass = 'gatubelo=00';
 
-		try{
-				$db = new PDO($link
-        ,$usuario, $pass);
 
-				echo 'Conectado'; 
+
+		// $link = 'mysql:host=localhost;dbname=mydb';
+		// $usuario =  'root';
+		// $pass = 'gatubelo=00';
+
+		// try{
+		// 		$db = new PDO($link
+  //       ,$usuario, $pass);
+
+		// 		echo 'Conectado'; 
 
 				
 
-		} catch(PDOException $e){
-			print "Error!: " .$e->getMessage()."<br/>";
-			die();
-		}
+		// } catch(PDOException $e){
+		// 	print "Error!: " .$e->getMessage()."<br/>";
+		// 	die();
+		// }
 
 
 
