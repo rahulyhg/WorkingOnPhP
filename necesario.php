a856e641b5140c66ab1b642c2021d11767d70407
ManuDeveloper <manuepeva@gmail.com>
2018-05-27 11:19:16 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -36,19 +36,7 @@
 <body>
 
 
-		<div style="margin:3em;"> 
-			<div><form action="necesario.php" method="post">
-			First Name: <font color=red*>*</font><input type="text" name="fname"><br />
-			Last Name: <font color=red*>*</font><input type="text" name="iname"><br />
-			About: <textarea name="about"></textarea><br />
-
-	<input type="reset" name="reset">
-
-	<input type="submit" name="submit" value="Go">
-</form>
-		<input type="hidden"  name="token" value="<?php echo $token;?>"/>
-</div>
-		</div>
+		
 
 </body>
 </html> -->
