7ef94abbd6517b1ffb07e0c7bc7cee7255a5f127
ManuDeveloper <manuepeva@gmail.com>
2018-05-29 19:58:06 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -63,6 +63,7 @@
 
 						<li>
 							<span class="item<?php echo $item['done'] ? ' done' : '' ?>	"><?php echo$item['username']; ?></span>
+							
 							<a href="#" class="done-button">Mark as done</a>
 						</li>
 					<?php endforeach; ?>
