0fe36b3b17270bbbf3176c8691949cf6372c2d58
ManuDeveloper <manuepeva@gmail.com>
2018-06-03 12:28:01 -0500

    commiting

--- index.php
+++ index.php
@@ -5,5 +5,17 @@
         'root', 'gatubelo=00')) {
             // echo "success";
         }
+       else {
+        echo "you are not logged!";
+       }
+      
+        // Handle this in some other way
+
+        if(!isset($_SESSION['user_id'])){
+            die('You are not signed in.');
+        }else
+        {
+            // echo "jodido!";
+        }
   
 ?>
