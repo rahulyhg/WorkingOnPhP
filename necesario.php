fb881670c2ccb9e5a3a18fbf5a24dd2a77180de7
ManuDeveloper <manuepeva@gmail.com>
2018-06-10 13:03:35 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -79,7 +79,7 @@
 				<?php endforeach; ?>
 					</ul>
 					<?php else: ?>
-			<p>You haven´t added any items yet.</p>
+						<p>You haven´t added any items yet.</p>
 				<?php endif; ?>
 			<form class="item-add" action"add.php" method="post">
 				<input type="text" name="name" placeholder="Type Your Name" class="">
