29d951338aaa49f6abeeabd0a4b7252cfec19dfc
ManuDeveloper <manuepeva@gmail.com>
2018-06-10 12:08:07 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -75,10 +75,12 @@
 					<a href="#" class="done-button"></a>
 				</li>
 			</ul>
+		<?php else: ?>
+			<p>You haven´t added any items yet.
 		<?php endif; ?>
 
 
-			<form class="item-add" action"add.php" method="post"><
+			<form class="item-add" action"add.php" method="post">
 				<input type="text" name="name" placeholder="Type Your Name" class="">
 				<input type="submit" value="Add" class="submit">
 			</form>	
