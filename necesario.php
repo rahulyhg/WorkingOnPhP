c215e10e86b01eb8a45c19139e2230daf9bd538a
ManuDeveloper <manuepeva@gmail.com>
2018-06-11 22:14:11 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -73,7 +73,7 @@
 
 				<?php foreach($items as $item): ?>
 						<li>
-							<span class="item<?php echo $item['user'] ? ' done' : '' ?>"><?php echo $item['user']; ?></span><br />
+							<span class="item<?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['user']; ?></span><br />
 							<?php if(!$item['user']): ?>
 							<a href="#" class="done-button">jodidos</a>
 								<?php endif; ?>
