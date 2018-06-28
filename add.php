a7974f2bb08f4d512e0a610c5c650a6ac9443387
ManuDeveloper <manuepeva@gmail.com>
2018-06-27 20:19:44 -0500

    commiting

--- add.php
+++ add.php
@@ -5,18 +5,6 @@ require_once'index.php';
 if(isset($_POST['name'])){
 	$name = trim($_POST['name']);
 
-	if (!empty($name)) {
-		$addedQuery = $mydb->prepare("
-				INSERT INTO jodidosTodos(id, user,  category)
-				values 
-				(:id, :user, :category NOW())
-			");
-
-		$addedQuery->execute([
-				'name' => $name, 
-				'user' => $_SESSION['user_id']
-		]);
-	}
 }
 
 header('Location: index.php');
