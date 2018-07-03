c9c380a4347ffbf66b3d51d8482029517bff751b
ManuDeveloper <manuepeva@gmail.com>
2018-07-02 20:06:23 -0500

    commiting

--- index.php
+++ index.php
@@ -2,5 +2,14 @@
         session_start();
         $_SESSION['user_id'] = 1;
         if ($db = new PDO('mysql:host=localhost;dbname=mydb;', 
-c
+        'root', 'gatubelo=00')) {
+            // echo "success";
+        }
+       else {
+        echo "you are not logged!";
+       }
+      
+        // Handle this in some other way
+
+ 
 ?>      
