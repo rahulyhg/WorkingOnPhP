d4ea6cf520955024776bfeb665c474eeda2963b8
ManuDeveloper <manuepeva@gmail.com>
2018-07-03 23:59:56 -0500

    m

--- index.php
+++ index.php
@@ -2,14 +2,4 @@
         session_start();
         $_SESSION['user_id'] = 1;
         if ($db = new PDO('mysql:host=localhost;dbname=mydb;', 
-        'root', 'gatubelo=00')) {
-            // echo "success";
-        }
-       else {
-        echo "you are not logged!";
-       }
-      
-        // Handle this in some other way
-
- 
-?>      
+        'root
