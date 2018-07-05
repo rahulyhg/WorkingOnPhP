dc8707a17b151ea873530c20c594e6d0bb3990c2
ManuDeveloper <manuepeva@gmail.com>
2018-07-04 22:27:35 -0500

    commiting

--- index.php
+++ index.php
@@ -3,3 +3,24 @@
         $_SESSION['user_id'] = 1;
         if ($db = new PDO('mysql:host=localhost;dbname=mydb;', 
         'root
+
+         session_start();
+        $_SESSION['user_id'] = 1;
+        if ($db = new PDO('mysql:host=localhost;dbname=mydb;', 
+        'root
+
+
+         session_start();
+        $_SESSION['user_id'] = 1;
+        if ($db = new PDO('mysql:host=localhost;dbname=mydb;', 
+        'root
+
+         session_start();
+        $_SESSION['user_id'] = 1;
+        if ($db = new PDO('mysql:host=localhost;dbname=mydb;', 
+        'root
+
+         session_start();
+        $_SESSION['user_id'] = 1;
+        if ($db = new PDO('mysql:host=localhost;dbname=mydb;', 
+        'root
