402a9a4bf24441fc7bddc19458771a7c608d6dc4
ManuDeveloper <manuepeva@gmail.com>
2018-07-06 19:59:58 -0500

    commiting

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
         session_start();
         $_SESSION['user_id'] = 1;
 
-        $db = new PDO('mysql:host=ManuP;dbname=employees', 
+        $db = new PDO('mysql:host=localhost;dbname=employees', 
         'root', 'gatubelo=00');
 
         // Handle this in other way 
