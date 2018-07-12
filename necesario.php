a5fa60473bc6f5dcc5d7e32e6b668a6269155c46
ManuDeveloper <manuepeva@gmail.com>
2018-07-11 23:01:46 -0500

    done item not working with php

--- necesario.php
+++ necesario.php
@@ -125,7 +125,7 @@
 
 			<ul class="items" >	
 
-				<?php foreach($item as $items): ?>
+				<?php foreach($items as $items): ?>
 						<li>
 							<span class="item<?php echo $item['category'] ? ' user' : '' ?>"><?php echo $item['user']; ?></span><br />
 							<?php if(!$item['category']): ?>
