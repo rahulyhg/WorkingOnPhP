a919913ff4009156f9afa8d161efe933a67fdc85
ManuDeveloper <manuepeva@gmail.com>
2018-07-17 20:24:14 -0500

    commiting

--- necesario.php
+++ necesario.php
@@ -98,8 +98,8 @@
 							Mark As done</a>
 							 <?php endif; ?> 
 						</li>
-						<!-- <div style="background-color: grey;">
-						<!-- 	<?php  $items?> -->
+						<div style="background-color: grey;">
+						<!-- 	<?php  $items?>
 						<!-- </div> --> 
 				<?php endforeach; ?>
 
